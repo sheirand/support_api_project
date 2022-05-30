7d79b5daabd279ac2297db520fe967b059281c72
evos <eugene.osakovich@gmail.com>
2022-05-30 17:13:35 +0300

    updated permissions 1. permissions.py:  - 2 custom object level permission are added 2. views.py:  - experiments with permissions

--- support_api/support/views.py
+++ support_api/support/views.py
@@ -1,22 +1,32 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets, mixins
-from rest_framework.permissions import IsAdminUser, AllowAny
+from rest_framework.permissions import IsAdminUser, AllowAny, IsAuthenticated
 from rest_framework.viewsets import GenericViewSet
 
 from support.models import Issue, Comments
 from support.serializers import IssueSerializer, IssueStatusSerializer
 from rest_framework.views import APIView, Response
-
+from support.permissions import IsOwnerOrStaff, IsStaff
 
 
 class CreateListRetrieveViewSet(viewsets.ModelViewSet):
-    queryset = Issue.objects.all()
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            pk = self.kwargs.get("pk")
+
+            if not pk:
+                return Issue.objects.all()
+
+            return Issue.objects.filter(pk=pk)
+        else:
+            return Issue.objects.none()
 
     def get_serializer_class(self):
-        if self.action in ['create', 'list', 'retrieve']:
-            serializer_class = IssueSerializer
-        else:
+        if self.action in ['update', 'partially_update',
+                           'destroy']:
             serializer_class = IssueStatusSerializer
+        else:
+            serializer_class = IssueSerializer
 
         return serializer_class
 
@@ -24,7 +34,12 @@ class CreateListRetrieveViewSet(viewsets.ModelViewSet):
         """
         Instantiates and returns the list of permissions that this view requires.
         """
-        if self.action == 'create':
+        if self.action == 'retrieve':
+            permission_classes = [IsOwnerOrStaff]
+        elif self.action in ['update', 'list', 'create'
+                             'partial_update', 'destroy']:
+            permission_classes = [AllowAny]
+        elif self.action == 'destroy':
             permission_classes = [IsAdminUser]
         else:
             permission_classes = [AllowAny]
